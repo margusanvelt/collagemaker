28be9d3b314370de5d6863c9d2841cd3abd65487
margusanvelt <margus.anvelt@gmail.com>
2015-12-09 11:42:34 +0200

    Update collagemaker.py

--- collagemaker.py
+++ collagemaker.py
@@ -10,7 +10,7 @@ import PIL
 
 location = raw_input("Tell me the path to your files\n i.e. /home/user/folder/")
 
-pildid = glob.glob(location + "*.JPG")
+pics = glob.glob(location + "*.JPG")
 CANVAS_X=input("Tell me the width:")
 try:
     CANVAS_Y=input("Tell me the height:")
@@ -24,35 +24,35 @@ def feeder():
     while len(pildid)>=4:
         corners = CORNERS[:]
         print corners
-        kanvas = Image.new('RGBA', (CANVAS_X, CANVAS_Y))
+        canvas = Image.new('RGBA', (CANVAS_X, CANVAS_Y))
         
         # Siin teeme neli pilti mis l2hevad nelikusse
         for x in range(0,4):
-            pilt = Image.open(pildid.pop(0))
-            if pilt.height > pilt.width:
-                pilt = pilt.rotate(90, expand = 1)
+            pic = Image.open(pics.pop(0))
+            if pic.height > pic.width:
+                pic = pic.rotate(90, expand = 1)
             #CHECK IF THE PICTURE PASSES THE SIZE REQUIREMENT, IF SO RUNS RESIZEIMAGE
-            if pilt.size[0] >= CANVAS_X/2:
-                pilt = resizeimage.resize_contain(pilt,[CANVAS_X/2, CANVAS_Y/2])
+            if pic.size[0] >= CANVAS_X/2:
+                pic = resizeimage.resize_contain(pic,[CANVAS_X/2, CANVAS_Y/2])
             #IF THE REQUIREMENT IS NOT MET, THE PICTURE WILL BE UPSCALED
             else:           
-                if pilt.height == pilt.width or pilt.height/CANVAS_Y > pilt.width/CANVAS_Y:
+                if pic.height == pic.width or pic.height/CANVAS_Y > pic.width/CANVAS_Y:
                     baseheight = CANVAS_Y/2
                     wpercent = (baseheight/float(pilt.size[0]))
                     hsize = int((float(pilt.size[1])*float(wpercent)))
-                    pilt = pilt.resize((baseheight,hsize), PIL.Image.ANTIALIAS) 
+                    pic = pic.resize((baseheight,hsize), PIL.Image.ANTIALIAS) 
                 else:
                     basewidth = CANVAS_X/2
                     wpercent = (basewidth/float(pilt.size[0]))
                     hsize = int((float(pilt.size[1])*float(wpercent)))
-                    pilt = pilt.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+                    pic = pic.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
 
 
 
-            kanvas.paste(pilt, corners.pop(0))
-        kanvas.save('/tmp/collab{0}.jpg'.format(counter))
+            canvas.paste(pic, corners.pop(0))
+        canvas.save('/tmp/collab{0}.jpg'.format(counter))
         counter += 1
-        print kanvas
+        print canvas
 feeder()    
 
 """basewidth = CANVAS_X/2
